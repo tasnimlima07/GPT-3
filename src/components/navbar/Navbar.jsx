a0db0b82ebc2ae82f39d999fbb1ee2d1e212fedb
tasnimlima07 <tasnimlima07@gmail.com>
2024-03-13 23:59:57 +0600

    added nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,5 @@
 import React from 'react'
 import './navbar.css';
-
-const Navbar = () => {
-  const[toggleMenu, setToggleMenu] = useState(false)
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
-
-export default Navbar
 import React, {useState} from 'react';
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from '../../assets/GPT-3.png';
