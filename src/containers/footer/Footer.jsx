ae17cd62592bb55f5f054ffcf46e4de21407778c
tasnimlima07 <tasnimlima07@gmail.com>
2024-03-22 15:28:16 +0600

    'footer'

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import './footer.css'
+import logo from '../../assets/GPT-3.png';
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privecy Policy</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085132567</p>
+          <p>info@pyme.net</p>
+          <p>Contact</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>&#169; 2024 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   )
 }
